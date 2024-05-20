3ede87ffe0adec43041b97ca364f3597e18fae32
sireesha-592 <psirisha592@gmail.com>
2024-05-20 18:32:33 +0530

    first commit

---
+++ src/main/java/in/siri/app/Siri1.java
@@ -0,0 +1,18 @@
+package in.siri.app;
+
+public class Siri1 {
+       String fname;
+       String lname;
+       String mobileNo;
+       
+       public Siri1() {
+    	   
+       }
+       
+       public Siri1(String fname,String lname,String mobileNo) {
+    	   
+    	   this.fname = fname;
+    	   this.lname = lname;
+    	   this.mobileNo = mobileNo;
+    	   }
+}
